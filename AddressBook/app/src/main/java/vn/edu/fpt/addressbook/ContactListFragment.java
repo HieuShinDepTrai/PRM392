50f90a5ee4eee8f86c744877e757d89bdc1ff3b8
HieuShinDepTrai <90301374+HieuShinDepTrai@users.noreply.github.com>
2023-10-20 11:05:36 +0700

    first upload

---
+++ AddressBook/app/src/main/java/vn/edu/fpt/addressbook/ContactListFragment.java
@@ -0,0 +1,158 @@
+// ContactListFragment.java
+// Displays the list of contact names
+package vn.edu.fpt.addressbook;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class ContactListFragment extends ListFragment
+{
+   // callback methods implemented by MainActivity  
+   public interface ContactListFragmentListener
+   {
+      // called when user selects a contact
+      public void onContactSelected(long rowID);
+
+      // called when user decides to add a contact
+      public void onAddContact();
+   }
+   
+   private ContactListFragmentListener listener; 
+   
+   private ListView contactListView; // the ListActivity's ListView
+   private CursorAdapter contactAdapter; // adapter for ListView
+   
+   // set ContactListFragmentListener when fragment attached   
+   @Override
+   public void onAttach(Activity activity)
+   {
+      super.onAttach(activity);
+      listener = (ContactListFragmentListener) activity;
+   }
+
+   // remove ContactListFragmentListener when Fragment detached
+   @Override
+   public void onDetach()
+   {
+      super.onDetach();
+      listener = null;
+   }
+
+   // called after View is created
+   @Override
+   public void onViewCreated(View view, Bundle savedInstanceState)
+   {
+      super.onViewCreated(view, savedInstanceState);
+      setRetainInstance(true); // save fragment across config changes
+      setHasOptionsMenu(true); // this fragment has menu items to display
+
+      // set text to display when there are no contacts
+      setEmptyText(getResources().getString(R.string.no_contacts));
+
+      // get ListView reference and configure ListView
+      contactListView = getListView(); 
+      contactListView.setOnItemClickListener(viewContactListener);      
+      contactListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+      
+      // map each contact's name to a TextView in the ListView layout
+      String[] from = new String[] { "name" };
+      int[] to = new int[] { android.R.id.text1 };
+      contactAdapter = new SimpleCursorAdapter(getActivity(), 
+         android.R.layout.simple_list_item_1, null, from, to, 0);
+      setListAdapter(contactAdapter); // set adapter that supplies data
+   }
+
+   // responds to the user touching a contact's name in the ListView
+   OnItemClickListener viewContactListener = new OnItemClickListener() 
+   {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, 
+         int position, long id) 
+      {
+         listener.onContactSelected(id); // pass selection to MainActivity
+      } 
+   }; // end viewContactListener
+
+   // when fragment resumes, use a GetContactsTask to load contacts 
+   @Override
+   public void onResume() 
+   {
+      super.onResume(); 
+      new GetContactsTask().execute((Object[]) null);
+   }
+
+   // performs database query outside GUI thread
+   private class GetContactsTask extends AsyncTask<Object, Object, Cursor> 
+   {
+      DatabaseConnector databaseConnector = 
+         new DatabaseConnector(getActivity());
+
+      // open database and return Cursor for all contacts
+      @Override
+      protected Cursor doInBackground(Object... params)
+      {
+         databaseConnector.open();
+         return databaseConnector.getAllContacts(); 
+      } 
+
+      // use the Cursor returned from the doInBackground method
+      @Override
+      protected void onPostExecute(Cursor result)
+      {
+         contactAdapter.changeCursor(result); // set the adapter's Cursor
+         databaseConnector.close();
+      } 
+   } // end class GetContactsTask
+
+   // when fragment stops, close Cursor and remove from contactAdapter 
+   @Override
+   public void onStop() 
+   {
+      Cursor cursor = contactAdapter.getCursor(); // get current Cursor
+      contactAdapter.changeCursor(null); // adapter now has no Cursor
+      
+      if (cursor != null) 
+         cursor.close(); // release the Cursor's resources
+      
+      super.onStop();
+   } 
+
+   // display this fragment's menu items
+   @Override
+   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+   {
+      super.onCreateOptionsMenu(menu, inflater);
+      inflater.inflate(R.menu.fragment_contact_list_menu, menu);
+   }
+
+   // handle choice from options menu
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) 
+   {
+      if(item.getItemId()==R.id.action_add)
+      {
+            listener.onAddContact();
+            return true;
+      }
+      
+      return super.onOptionsItemSelected(item); // call super's method
+   }
+   
+   // update data set
+   public void updateContactList()
+   {
+      new GetContactsTask().execute((Object[]) null);
+   }
+} // end class ContactListFragment
---
+++ CannonGame/app/src/main/java/vn/edu/fpt/cannongame/Line.java
@@ -0,0 +1,8 @@
+package vn.edu.fpt.cannongame;
+
+import android.graphics.Point;
+
+public class Line {
+    public Point start = new Point(); // start Point--(0,0) by default
+    public Point end = new Point(); // end Point--(0,0) by default
+}
